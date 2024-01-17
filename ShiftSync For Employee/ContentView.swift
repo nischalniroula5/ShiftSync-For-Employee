101115093868b8df018dc5f539683385948ddcec
Nischal Niroula <officialnischalniroula@gmail.com>
2024-01-17 23:56:09 +1100

    Initial Commit

---
+++ ShiftSync For Employee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShiftSync For Employee
+//
+//  Created by Nischal Niroula on 17/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShiftSync For Employee/ShiftSync_For_EmployeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftSync_For_EmployeeApp.swift
+//  ShiftSync For Employee
+//
+//  Created by Nischal Niroula on 17/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShiftSync_For_EmployeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
